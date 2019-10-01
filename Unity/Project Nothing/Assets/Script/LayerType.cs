cc29c3a939c4138afd39f61c11986769bf1c290a
Eyilee <eyilee@protonmail.com>
2019-10-01 18:29:09 +0800

    [add] grid 點擊偵測 ing

---
+++ Unity/Project Nothing/Assets/Script/LayerType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectNothing
+{
+    public enum ELayerType
+    {
+        eLayer_Default = 0,
+        eLayer_TransparentFX = 1,
+        eLayer_IgnoreRaycast = 2,
+        eLayer_Water = 4,
+        eLayer_UI = 5,
+        eLayer_Map = 8
+    }
+}
--- Unity/Project Nothing/Assets/Script/SelectionManager.cs
+++ Unity/Project Nothing/Assets/Script/SelectionManager.cs
@@ -5,10 +5,11 @@ namespace ProjectNothing
 {
     public class SelectionManager : MonoSingleton<SelectionManager>
     {
-        public delegate void PointerEventHandler (BaseEventData baseEventData);
-        event PointerEventHandler OnLongPress;
-        event PointerEventHandler OnLongPressEnd;
-        event PointerEventHandler OnClick;
+        public delegate void PointerEventHandler (Vector2 point);
+        //event PointerEventHandler OnLongPress;
+        //event PointerEventHandler OnLongPressEnd;
+        //event PointerEventHandler OnClick;
+        event PointerEventHandler OnLayerMapPointerDown;
 
         // current scene
         // current state
@@ -28,8 +29,21 @@ namespace ProjectNothing
                 RaycastHit2D hit = Physics2D.Raycast (Input.mousePosition, Vector2.zero);
                 if (hit.collider != null)
                 {
-                    switch (hit.collider.gameObject.layer)
+                    switch ((ELayerType)hit.collider.gameObject.layer)
                     {
+                        case ELayerType.eLayer_Default:
+                            break;
+                        case ELayerType.eLayer_TransparentFX:
+                            break;
+                        case ELayerType.eLayer_IgnoreRaycast:
+                            break;
+                        case ELayerType.eLayer_Water:
+                            break;
+                        case ELayerType.eLayer_UI:
+                            break;
+                        case ELayerType.eLayer_Map:
+                            OnLayerMapPointerDown (hit.point);
+                            break;
                         default:
                             break;
                     }
@@ -38,6 +52,28 @@ namespace ProjectNothing
             else if (isLeftButtonPressed && !Input.GetMouseButton (0))
             {
                 isLeftButtonPressed = false;
+            }
+        }
+
+        public void AddLayerPointerDown (ELayerType eLayerType, PointerEventHandler pointerEventHandler)
+        {
+            switch (eLayerType)
+            {
+                case ELayerType.eLayer_Default:
+                    break;
+                case ELayerType.eLayer_TransparentFX:
+                    break;
+                case ELayerType.eLayer_IgnoreRaycast:
+                    break;
+                case ELayerType.eLayer_Water:
+                    break;
+                case ELayerType.eLayer_UI:
+                    break;
+                case ELayerType.eLayer_Map:
+                    OnLayerMapPointerDown += pointerEventHandler;
+                    break;
+                default:
+                    break;
             }
         }
 
