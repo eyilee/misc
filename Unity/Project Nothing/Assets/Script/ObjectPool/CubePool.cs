cadfdb9f48abd4b3834e76af9f3c7465c46a6528
Eyilee <eyilee@protonmail.com>
2019-11-14 17:59:07 +0800

    [add] ObjectPool

---
+++ Unity/Project Nothing/Assets/Script/ObjectPool/CubePool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CubePool : ObjectPool<CubePool, CubeObject>
+{
+}
+
+public class CubeObject : PoolObject<CubePool, CubeObject>
+{
+}
