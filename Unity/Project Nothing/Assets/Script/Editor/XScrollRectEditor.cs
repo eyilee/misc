0d4b137becc8cbfd8addf4fc942edb748e17e2fe
Eyilee <eyilee@protonmail.com>
2020-02-15 17:15:09 +0800

    [add] XScrollRect

---
+++ Unity/Project Nothing/Assets/Script/Editor/XScrollRectEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+[CustomEditor (typeof (XScrollRect))]
+public class XScrollRectEditor : ScrollRectEditor
+{
+    SerializedProperty layoutTypeProp;
+    SerializedProperty columnProp;
+    SerializedProperty rowProp;
+    SerializedProperty defaultItemProp;
+
+    protected override void OnEnable ()
+    {
+        base.OnEnable ();
+
+        layoutTypeProp = serializedObject.FindProperty ("layoutType");
+        columnProp = serializedObject.FindProperty ("column");
+        rowProp = serializedObject.FindProperty ("row");
+        defaultItemProp = serializedObject.FindProperty ("defaultItem");
+    }
+
+    public override void OnInspectorGUI ()
+    {
+        base.OnInspectorGUI ();
+
+        serializedObject.Update ();
+
+        EditorGUILayout.Space ();
+        EditorGUILayout.PropertyField (layoutTypeProp, new GUIContent ("Layout Type"));
+
+        EditorGUI.indentLevel++;
+        switch ((XScrollRect.LayoutType)layoutTypeProp.enumValueIndex)
+        {
+            case XScrollRect.LayoutType.Horizontal:
+                EditorGUILayout.PropertyField (columnProp, new GUIContent ("Column Size"));
+                break;
+            case XScrollRect.LayoutType.Vertical:
+                EditorGUILayout.PropertyField (rowProp, new GUIContent ("Row Size"));
+                break;
+            case XScrollRect.LayoutType.Grid:
+                EditorGUILayout.PropertyField (columnProp, new GUIContent ("Column Size"));
+                EditorGUILayout.PropertyField (rowProp, new GUIContent ("Row Size"));
+                break;
+        }
+        EditorGUI.indentLevel--;
+
+        EditorGUILayout.Space ();
+        EditorGUILayout.PropertyField (defaultItemProp, new GUIContent ("Default Item"));
+
+        serializedObject.ApplyModifiedProperties ();
+    }
+}
---
+++ Unity/Project Nothing/Assets/Script/XScrollRect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class XScrollRect : ScrollRect
+{
+    public enum LayoutType
+    {
+        Horizontal,
+        Vertical,
+        Grid
+    }
+
+    public LayoutType layoutType;
+    public int column;
+    public int row;
+
+    public GameObject defaultItem;
+
+    class ItemInfo
+    {
+
+    }
+
+    public override void OnScroll (PointerEventData data)
+    {
+        base.OnScroll (data);
+    }
+}
