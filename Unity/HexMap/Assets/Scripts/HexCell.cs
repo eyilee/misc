3ad992ace863c9f556125208778d4c88bd0ac796
Eyilee <eyilee@protonmail.com>
2020-07-09 14:53:16 +0800

    [add] HexMap

---
+++ Unity/HexMap/Assets/Scripts/HexCell.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+    public HexCoordinates coordinates;
+
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
