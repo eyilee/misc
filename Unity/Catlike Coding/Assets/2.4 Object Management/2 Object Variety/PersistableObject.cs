212b1c7c604a46d65f2090adc8cf89e6313faa82
Eyilee <eyilee@protonmail.com>
2021-01-20 17:50:07 +0800

    [add] Catlike Coding Object Management Object Variety

---
+++ Unity/Catlike Coding/Assets/2.4 Object Management/2 Object Variety/PersistableObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ObjectManagement
+{
+    namespace ObjectVariety
+    {
+        [DisallowMultipleComponent]
+        public class PersistableObject : MonoBehaviour
+        {
+            public virtual void Save (GameDataWriter writer)
+            {
+                writer.Write (transform.localPosition);
+                writer.Write (transform.localRotation);
+                writer.Write (transform.localScale);
+            }
+
+            public virtual void Load (GameDataReader reader)
+            {
+                transform.localPosition = reader.ReadVector3 ();
+                transform.localRotation = reader.ReadQuaternion ();
+                transform.localScale = reader.ReadVector3 ();
+            }
+        }
+    }
+}
---
+++ Unity/Catlike Coding/Assets/2.4 Object Management/2 Object Variety/ShapeFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace ObjectManagement
+{
+    namespace ObjectVariety
+    {
+        [CreateAssetMenu]
+        public class ShapeFactory : ScriptableObject
+        {
+            [SerializeField]
+            Shape[] prefabs = null;
+
+            [SerializeField]
+            Material[] materials = null;
+
+            public Shape Get (int shapeId = 0, int materialId = 0)
+            {
+                Shape instance = Instantiate (prefabs[shapeId]);
+                instance.ShapeId = shapeId;
+                instance.SetMaterial (materials[materialId], materialId);
+                return instance;
+            }
+
+            public Shape GetRandom ()
+            {
+                return Get (
+                    Random.Range (0, prefabs.Length),
+                    Random.Range (0, materials.Length)
+                );
+            }
+        }
+    }
+}
