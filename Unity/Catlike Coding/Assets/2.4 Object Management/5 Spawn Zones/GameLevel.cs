0bdd415b237f64c699f75af964639086f0d1944e
Eyilee <eyilee@protonmail.com>
2021-05-03 15:21:23 +0800

    [add] Catlike Coding Object Management Spawn Zones

---
+++ Unity/Catlike Coding/Assets/2.4 Object Management/5 Spawn Zones/GameLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ObjectManagement
+{
+    namespace SpawnZones
+    {
+        public class GameLevel : MonoBehaviour
+        {
+            [SerializeField]
+            SpawnZone spawnZone = null;
+
+            void Start ()
+            {
+                Game.Instance.SpawnZoneOfLevel = spawnZone;
+            }
+        }
+    }
+}
---
+++ Unity/Catlike Coding/Assets/2.4 Object Management/5 Spawn Zones/SpawnZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ObjectManagement
+{
+    namespace SpawnZones
+    {
+        public abstract class SpawnZone : MonoBehaviour
+        {
+            public abstract Vector3 SpawnPoint { get; }
+        }
+    }
+}
